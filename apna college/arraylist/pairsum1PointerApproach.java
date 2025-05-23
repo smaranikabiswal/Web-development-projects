55e6b6dc55ba5a78a53cf6643d2b33ab711b7a4c
smaranika biswal <23btcse80@suiit.ac.in>
2025-05-23 18:38:23 +0530

    new programs

---
+++ apna college/arraylist/pairsum1PointerApproach.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class pairsum1PointerApproach{
+    public static boolean sum(ArrayList<Integer>list,int target){
+        int lp=0;
+        int rp=list.size()-1;
+        while (lp!=rp) { 
+            if (list.get(lp)+list.get(rp)==target) {
+                return true;
+            }if(list.get(lp)+list.get(rp)<target){
+                lp++;
+            
+            } else {
+                rp--;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+         ArrayList<Integer> list=new ArrayList<>();     
+list.add(1);
+list.add(2);
+list.add(3);
+list.add(4);
+list.add(5); 
+int target=90;
+System.out.println(sum(list,target));
+    }
+}
