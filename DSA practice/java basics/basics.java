d67b8bcb22d06e5ff73dd7bdf814bafb6639aa44
smaranika biswal <23btcse80@suiit.ac.in>
2026-01-17 21:30:50 +0530

    Delete DSA practice directory

--- DSA practice/java basics/basics.java
+++
@@ -1,7 +0,0 @@
-public class basics{
-public static void main(String args[]){
-    System.out.println("hello world");
-    System.out.println("hello world");
-    System.out.println("hello world");
-}
-}
