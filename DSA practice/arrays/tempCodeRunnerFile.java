d27603db71a9b89b1407e617e7a4cb4ecf7f7fa3
smaranika biswal <23btcse80@suiit.ac.in>
2026-02-14 14:21:34 +0530

    Delete DSA practice directory

--- DSA practice/arrays/tempCodeRunnerFile.java
+++
@@ -1,5 +0,0 @@
-public static void main(String args[]){
-    int numbers[]={2,4,6,8,10};
-    printpairs(numbers);
-}
-}
--- DSA practice/bit manipulation/clearbits.java
+++
@@ -1,8 +0,0 @@
-public class clearbits{
-public static int clrbits(int n,int i){
-    return n & ((-1)<<i);
-}
-public static void main(String[] args) {
-  System.out.println(clrbits(5,1));  
-}
-}
